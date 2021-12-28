f979e8ddd4221df244538eec8c3191be9a6f43bf
Roman <Busnab@mail.ru>
2021-12-28 18:11:01 +0300

    try kafka

--- src/main/java/com/mastery/java/task/WebApp.java
+++ src/main/java/com/mastery/java/task/WebApp.java
@@ -2,9 +2,18 @@ package com.mastery.java.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
+@EnableKafka
 public class WebApp {
+
+    @KafkaListener(topics="test")
+    public void msgListener(String msg){
+        System.out.println(msg);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);
     }
