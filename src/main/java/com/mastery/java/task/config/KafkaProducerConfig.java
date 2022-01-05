1dad31f7b04ffd5e645f6e521643be70c6dda098
Roman <Busnab@mail.ru>
2022-01-05 17:48:58 +0300

    add spring cloud config server

--- src/main/java/com/mastery/java/task/config/KafkaProducerConfig.java
+++ src/main/java/com/mastery/java/task/config/KafkaProducerConfig.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@RefreshScope
 public class KafkaProducerConfig {
     @Value("${kafka.server}")
     private String kafkaServer;
